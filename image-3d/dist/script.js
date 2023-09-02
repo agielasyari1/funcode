ac668d8d5b92f34be7dc040c80caa7a0b538b028
agielasyari1 <agielasyari1@gmail.com>
2023-09-02 19:34:10 +0700

    change

--- image-3d/dist/script.js
+++ image-3d/dist/script.js
@@ -1,32 +1,9 @@
-// Author: Hoang Tran (https://fb.com/99.hoangtran)
+var radius = 240; 
+var autoRotate = true; 
+var rotateSpeed = -60; 
+var imgWidth = 120; 
+var imgHeight = 170; 
 
-// Github verson (1 file .html): https://github.com/HoangTran0410/3DCarousel/blob/master/index.html
-
-// Give me a coffee <3 https://github.com/HoangTran0410/HoangTran0410/blob/main/DONATE.md
-
-
-// You can change global variables here:
-var radius = 240; // how big of the radius
-var autoRotate = true; // auto rotate or not
-var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
-
-// Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
-var bgMusicControls = true; // Show UI music control
-
-/*
-     NOTE:
-       + imgWidth, imgHeight will work for video
-       + if imgWidth, imgHeight too small, play/pause button in <video> will be hidden
-       + Music link are taken from: https://hoangtran0410.github.io/Visualyze-design-your-own-/?theme=HauMaster&playlist=1&song=1&background=28
-       + Custom from code in tiktok video  https://www.facebook.com/J2TEAM.ManhTuan/videos/1353367338135935/
-*/
-
-
-// ===================== start =======================
-// animation start after 1000 miliseconds
 setTimeout(init, 1000);
 
 var odrag = document.getElementById('drag-container');
@@ -35,11 +12,9 @@ var aImg = ospin.getElementsByTagName('img');
 var aVid = ospin.getElementsByTagName('video');
 var aEle = [...aImg, ...aVid]; // combine 2 arrays
 
-// Size of images
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
 
-// Size of ground - depend on radius
 var ground = document.getElementById('ground');
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
@@ -53,7 +28,6 @@ function init(delayTime) {
 }
 
 function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
   if(tY > 180) tY = 180;
   if(tY < 0) tY = 0;
 
@@ -70,14 +44,11 @@ var sX, sY, nX, nY, desX = 0,
     tX = 0,
     tY = 10;
 
-// auto spin
 if (autoRotate) {
   var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
   ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
 }
 
-
-// setup events
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
